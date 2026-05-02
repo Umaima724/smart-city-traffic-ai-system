7baa0eeabfc43acf2fa7455cc94d7e98546e8733
Umaima724 <umaimamubashar19@gmail.com>
2026-05-02 15:31:55 +0500

    Update __init__.py

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -5,5 +5,16 @@ Makes module classes available at package level.
 
 from src.modules.input_preprocessing import InputPreprocessingModule
 from src.modules.request_router import RequestRouter
+from src.modules.ann_priority import (
+    ANNPriorityModule,
+    BinaryPriorityClassifier,
+    MultiClassPriorityMLP
+)
 
-__all__ = ['InputPreprocessingModule', 'RequestRouter']
+__all__ = [
+    'InputPreprocessingModule',
+    'RequestRouter',
+    'ANNPriorityModule',
+    'BinaryPriorityClassifier',
+    'MultiClassPriorityMLP'
+]
